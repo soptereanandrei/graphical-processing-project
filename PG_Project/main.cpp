e446159a32be773f8a4f8ebe879be1216c4625ad
Sopterean Andrei-Mihai <soptereanandrei@yahoo.com>
2022-01-07 13:53:25 +0200

    Init OpenGL application from template

---
+++ PG_Project/main.cpp
@@ -0,0 +1,318 @@
+//
+//  main.cpp
+//  OpenGL_Shader_Example_step1
+//
+//  Created by CGIS on 02/11/16.
+//  Copyright � 2016 CGIS. All rights reserved.
+//
+
+#define GLEW_STATIC
+
+#include <iostream>
+#include "glm/glm.hpp"//core glm functionality
+#include "glm/gtc/matrix_transform.hpp"//glm extension for generating common transformation matrices
+#include "glm/gtc/type_ptr.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include "Shader.hpp"
+#include "Camera.hpp"
+#define TINYOBJLOADER_IMPLEMENTATION
+
+#include "Model3D.hpp"
+#include "Mesh.hpp"
+
+int glWindowWidth = 640;
+int glWindowHeight = 480;
+int retina_width, retina_height;
+GLFWwindow* glWindow = NULL;
+
+glm::mat4 model;
+GLint modelLoc;
+
+glm::mat4 view;
+GLint viewLoc;
+
+glm::vec3 camPosition = glm::vec3(0.0f, 5.0f, 15.0f);
+glm::vec3 camTarget = glm::vec3(0.0f, 2.0f, -10.0f);
+
+//gps::Camera myCamera(glm::vec3(0.0f, 5.0f, 15.0f), glm::vec3(0.0f, 2.0f, -10.0f));
+float cameraSpeed = 0.01f;
+
+bool pressedKeys[1024];
+float angle = 0.0f;
+
+gps::Model3D myModel;
+gps::Shader myCustomShader;
+
+void windowResizeCallback(GLFWwindow* window, int width, int height)
+{
+	fprintf(stdout, "window resized to width: %d , and height: %d\n", width, height);
+	//TODO
+}
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			pressedKeys[key] = true;
+		else if (action == GLFW_RELEASE)
+			pressedKeys[key] = false;
+	}
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+
+}
+
+void processMovement()
+{
+
+	if (pressedKeys[GLFW_KEY_Q]) {
+		angle += 0.0002;
+	}
+
+	if (pressedKeys[GLFW_KEY_E]) {
+		angle -= 0.0002;
+	}
+
+	if (pressedKeys[GLFW_KEY_W]) {
+		//myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
+		
+	}
+
+	if (pressedKeys[GLFW_KEY_S]) {
+		//myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
+	}
+
+	if (pressedKeys[GLFW_KEY_A]) {
+		//myCamera.move(gps::MOVE_LEFT, cameraSpeed);
+	}
+
+	if (pressedKeys[GLFW_KEY_D]) {
+		//myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
+	}
+
+	if (pressedKeys[GLFW_KEY_UP]) {
+		camPosition.z -= 0.1;
+		camTarget.z -= 0.1;
+	}
+	if (pressedKeys[GLFW_KEY_DOWN]) {
+		camPosition.z += 0.1;
+		camTarget.z += 0.1;
+	}
+}
+
+bool initOpenGLWindow()
+{
+	if (!glfwInit()) {
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return false;
+	}
+
+	//for Mac OS X
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glWindow = glfwCreateWindow(glWindowWidth, glWindowHeight, "OpenGL Shader Example", NULL, NULL);
+	if (!glWindow) {
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return false;
+	}
+
+	glfwSetWindowSizeCallback(glWindow, windowResizeCallback);
+	glfwMakeContextCurrent(glWindow);
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+	// start GLEW extension handler
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// get version info
+	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+	const GLubyte* version = glGetString(GL_VERSION); // version as a string
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+
+	//for RETINA display
+	glfwGetFramebufferSize(glWindow, &retina_width, &retina_height);
+
+	glfwSetKeyCallback(glWindow, keyboardCallback);
+	glfwSetCursorPosCallback(glWindow, mouseCallback);
+    //glfwSetInputMode(glWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	return true;
+}
+
+GLuint ReadTextureFromFile(const char* file_name) {
+	int x, y, n;
+	int force_channels = 4;
+	unsigned char* image_data = stbi_load(file_name, &x, &y, &n, force_channels);
+	if (!image_data) {
+		fprintf(stderr, "ERROR: could not load %s\n", file_name);
+		return false;
+	}
+	// NPOT check
+	if ((x & (x - 1)) != 0 || (y & (y - 1)) != 0) {
+		fprintf(
+			stderr, "WARNING: texture %s is not power-of-2 dimensions\n", file_name
+		);
+	}
+
+	int width_in_bytes = x * 4;
+	unsigned char *top = NULL;
+	unsigned char *bottom = NULL;
+	unsigned char temp = 0;
+	int half_height = y / 2;
+
+	for (int row = 0; row < half_height; row++) {
+		top = image_data + row * width_in_bytes;
+		bottom = image_data + (y - row - 1) * width_in_bytes;
+		for (int col = 0; col < width_in_bytes; col++) {
+			temp = *top;
+			*top = *bottom;
+			*bottom = temp;
+			top++;
+			bottom++;
+		}
+	}
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		GL_SRGB, //GL_SRGB,//GL_RGBA,
+		x,
+		y,
+		0,
+		GL_RGBA,
+		GL_UNSIGNED_BYTE,
+		image_data
+	);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	return textureID;
+}
+
+GLuint verticesVBO;
+GLuint verticesEBO;
+GLuint objectVAO;
+GLint texture;
+
+//vertex position and UV coordinates
+GLfloat vertexData[] = {
+	0
+};
+
+GLuint vertexIndices[] = {
+	0
+};
+
+
+void renderScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.8, 0.8, 0.8, 1.0);
+	glViewport(0, 0, retina_width, retina_height);
+
+	//initialize the model matrix
+	model = glm::mat4(1.0f);
+	modelLoc = glGetUniformLocation(myCustomShader.shaderProgram, "model");
+
+	processMovement();
+
+	myCustomShader.useShaderProgram();
+
+	//initialize the view matrix
+	
+	//send matrix data to shader
+	view = glm::lookAt(camPosition, camTarget, glm::vec3(0.0f, 1.0f, 0.0f));
+	GLint viewLoc = glGetUniformLocation(myCustomShader.shaderProgram, "view");
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+	//create rotation matrix
+	model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
+	//send matrix data to vertex shader
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+}
+
+void loadTriangleData() {
+	glGenVertexArrays(1, &objectVAO);
+
+	glBindVertexArray(objectVAO);
+
+	glGenBuffers(1, &verticesVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, verticesVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &verticesEBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, verticesEBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertexIndices), vertexIndices, GL_STATIC_DRAW);
+
+	//vertex position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//vertex texture
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	glBindVertexArray(0);
+}
+
+int main(int argc, const char * argv[]) {
+
+	initOpenGLWindow();
+
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+	glEnable(GL_CULL_FACE); // cull face
+	glCullFace(GL_BACK); // cull back face
+	glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
+
+	myCustomShader.loadShader("shaders/shaderStart.vert", "shaders/shaderStart.frag");
+	myCustomShader.useShaderProgram();
+
+
+	view = glm::lookAt(camPosition, camTarget, glm::vec3(0.0f, 1.0f, 0.0f));
+	viewLoc = glGetUniformLocation(myCustomShader.shaderProgram, "view");
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+	//initialize the projection matrix
+	glm::mat4 projection = glm::perspective(45.0f, (float)glWindowWidth / (float)glWindowHeight, 0.1f, 1000.0f);
+	//send matrix data to shader
+	GLint projLoc = glGetUniformLocation(myCustomShader.shaderProgram, "projection");
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+
+
+	while (!glfwWindowShouldClose(glWindow)) {
+		renderScene();
+
+		glfwPollEvents();
+		glfwSwapBuffers(glWindow);
+	}
+
+	//close GL context and any other GLFW resources
+	glfwTerminate();
+
+	return 0;
+}
